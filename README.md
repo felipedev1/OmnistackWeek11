1dc7dec4ada8271282c2752c737b9b276358e0f3
Felipe <felipe.pereira.dev@gmail.com>
2020-03-29 14:54:26 -0300

    adding automated tests and readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# BE THE HERO
+
+  The application aims to connect NGOs with people who are willing to help. NGOs register incidents on the website and in the mobile application, people can see the cases and help with a fee.
+  Project made by rocketseat during the week omnistack.
+## OmnistackWeek11
+
+Building a complete application with **Node.js** on the back end, **ReactJS** on the front end and **React Native** on mobile.
+
+## Getting Started
+
+#### To run the project, you will need to install the next programs:
+- IDE of your choice (optional), recommended: [VS Code Download](https://code.visualstudio.com/download).
+- The javascript interpreter: [NodeJS](https://nodejs.org/en/download/).
+- [Git: to download the source code files on your machine](https://git-scm.com/downloads).
+ Or you can also [download the source code zip](https://github.com/felipedev1/OmnistackWeek11/archive/master.zip).
+ 
+##### And if you haven't downloaded the zip you can run the next command in the *terminal promp*, in the directory of your choice:
+  ```git
+  $ git clone https://github.com/felipedev1/OmnistackWeek11.git
+  ```
+ ## Build
+ 
+#### To build the project you need to download all the dependencies of each project file by executing the next command in its respective directory:
+```
+npm install
+```
+
+## Run Project
+#### To execute the code just type the following commands in the directory of each project:
+```
+npm start
+```
+## Links
+- [Rockseat](https://rocketseat.com.br/)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,57 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
+
 const ongControler = require('./controllers/ongController')
 const incidentController = require('./controllers/incidentController')
 const profileController = require('./controllers/profileController')
 const sessionController = require('./controllers/sessionController')
+
 const routes = express.Router()
 
 routes.get('/ongs', ongControler.index)
-routes.post('/ongs', ongControler.create)
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), ongControler.create)
 
-routes.get('/incidents', incidentController.index)
-routes.post('/incidents', incidentController.create)
-routes.delete('/incidents/:id', incidentController.delete)
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), incidentController.index)
 
-routes.get('/profile', profileController.index)
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization:  Joi.string().required()
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string(),
+    value: Joi.number().required()
+  })
+}), incidentController.create)
 
-routes.post('/session', sessionController.create)
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), incidentController.delete)
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization:  Joi.string().required()
+  }).unknown()
+}), profileController.index)
+
+routes.post('/session', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), sessionController.create)
 
 module.exports = routes
